ce18b2967c5dae9c89183993524838a0e1f343e3
saadindictrans <saad.c@indictranstech.com>
2023-06-13 14:13:01 +0530

    feat:unit doctype from code insated of frontend

---
+++ catalyst_management/catalyst_management_services/doctype/unit/unit.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Simon Wanyama and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class Unit(NestedSet):
+	pass
