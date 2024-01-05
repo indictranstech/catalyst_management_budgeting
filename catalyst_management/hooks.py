05cbdcac3b39bbbafd05982e87b0157dc10575c2
kiranchoudhary123 <kiranchoudhary0808@gmail.com>
2024-01-05 17:03:50 +0530

    added member and member type field in journal entry form

--- catalyst_management/hooks.py
+++ catalyst_management/hooks.py
@@ -122,6 +122,9 @@ doctype_js = {
 doc_events = {
 	"Project": {
 		"before_save": "catalyst_management.custom_script.project.project_custom.date_log",
+	},
+	"Journal Entry": {
+		"before_save": "catalyst_management.custom_script.journal_entry.journal_entry.before_save",
 	}
 }
 
