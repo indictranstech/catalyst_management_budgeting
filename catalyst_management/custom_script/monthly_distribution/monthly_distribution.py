8b8832de8decfb7bbe72749ce61868e9a60fae65
kiranchoudhary123 <kiranchoudhary0808@gmail.com>
2024-01-31 18:46:05 +0530

    comment in monthly distribution file

--- catalyst_management/custom_script/monthly_distribution/monthly_distribution.py
+++ catalyst_management/custom_script/monthly_distribution/monthly_distribution.py
@@ -2,5 +2,5 @@ import frappe
 
 def before_save(doc,method):
     total_amount = sum(amt.amount_allocation for amt in doc.percentages)
-    # Update grand total in Monthly Distribution
+    # Update grand total in Monthly Distribution.
     doc.custom_grand_total = total_amount
