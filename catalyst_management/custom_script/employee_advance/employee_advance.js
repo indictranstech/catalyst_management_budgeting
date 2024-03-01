3d1e7f7825019e3dd6e2c8edd8938dab4cd1496c
kiranchoudhary123 <kiranchoudhary0808@gmail.com>
2024-03-01 11:02:47 +0530

    create employee advance folder and file

---
+++ catalyst_management/custom_script/employee_advance/employee_advance.js
@@ -0,0 +1,22 @@
+frappe.ui.form.on('Employee Advance', {
+    before_save(frm) {
+        console.log("!!!!!!!!!!!!!!!!!!!")
+            frappe.db.get_list('Contract', {
+            fields: ['end_date'],
+            filters: {party_name: frm.doc.employee, custom_project: frm.doc.project}
+        }).then(records => {
+            $.each(records, function(i, r) {
+                if (r.end_date < frm.doc.posting_date) {
+                    frm.set_df_property("custom_reason","hidden",0)
+                }
+            });
+        });
+      
+    },
+    refresh(frm){
+        if (frm.doc.custom_reason != null){
+            frm.set_df_property("custom_reason","hidden",0)
+        }
+
+    }
+});
