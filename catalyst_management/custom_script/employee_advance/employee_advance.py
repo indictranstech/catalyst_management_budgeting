28f6afef51ba9ff352bd9d7ac2419a053e52535b
kiranchoudhary123 <kiranchoudhary0808@gmail.com>
2024-03-04 16:41:24 +0530

    changes in notification popup

--- catalyst_management/custom_script/employee_advance/employee_advance.py
+++ catalyst_management/custom_script/employee_advance/employee_advance.py
@@ -17,14 +17,14 @@ def validate(doc, method):
             doc.custom_approved_on = doc.modified
 
 def validate_posting_date(doc, method):
-        contract_details = frappe.db.get_list('Contract', {'party_name': doc.employee, 'custom_project': doc.project}, ['end_date','name'])
+        contract_details = frappe.db.get_list('Contract', {'party_name': doc.employee, 'custom_project': doc.project}, ['end_date','name', ])
         for i in contract_details:
             if str(i.end_date) < str(doc.posting_date):
                 if not doc.custom_reason:
                     ms =f'''
                     <a href="{frappe.utils.get_url_to_form('Contract', i.name)}">{i.name}</a>
                     ''' 
-                    frappe.throw(frappe._("Contract has been Expired. Last Date is {0}. If you want to proceed then select reason.Contract is {1}.").format(formatdate(i.end_date), ms))   
+                    frappe.throw(frappe._("Contract has been Expired. Last Date is {0}. If you want to proceed then select reason.If needed kindly access the Contract here - {1}.").format(formatdate(i.end_date), ms))   
 
 
 
--- catalyst_management/custom_script/expense_claim/expense_claim.py
+++ catalyst_management/custom_script/expense_claim/expense_claim.py
@@ -28,21 +28,24 @@ def update_chart_of_account(doc, methode):
 
 def validate_posting_date(doc, method):
     for item in doc.expenses:
-        contract_details = frappe.db.get_list('Contract', {'party_name': doc.employee, 'custom_project': item.project}, ['end_date','name'])
+        contract_details = frappe.db.get_list('Contract', {'party_name': doc.employee, 'custom_project': item.project}, ['end_date','name', 'custom_amount'])
         for i in contract_details:
             if str(i.end_date) < str(doc.date):
                 if not doc.custom_reason:
                     ms =f'''
                     <a href="{frappe.utils.get_url_to_form('Contract', i.name)}">{i.name}</a>
                     ''' 
-                    frappe.throw(frappe._("Contract has been Expired. Last Date is {0}. If you want to proceed then select reason. Contract is {1}.").format(formatdate(i.end_date), ms))   
+                    frappe.throw(frappe._("Contract has been Expired. Last Date is {0}.If you want to proceed then select reason. If needed kindly access the Contract here - {1}.").format(formatdate(i.end_date), ms))   
 
 def calculate_items_amount(doc, method):
     for item in doc.expenses: 
         project_budgeting_amount = frappe.db.get_all("Project Budgeting", filters={"name": item.project_for_budget}, fields=["custom_grand_total","custom_total_actual_amount","name"])
         for pb in project_budgeting_amount:
-            if doc.custom_total_amount > pb.custom_grand_total: 
-                frappe.throw(frappe._("Total Consolidated Amount is greater than Grand Total."))
+            if doc.custom_total_amount > pb.custom_grand_total:
+                ms =f'''
+                    <a href="{frappe.utils.get_url_to_form('Project Budgeting', pb.name)}">{pb.name}</a>
+                    ''' 
+                frappe.throw(frappe._("Total Consolidated Amount is greater than Project Budget Grand Total. If needed kindly access the Project Budget here - {0}.").format(ms))
                 
 def update_project_budget_actual_amount(doc, method):
     for item in doc.expenses: 
