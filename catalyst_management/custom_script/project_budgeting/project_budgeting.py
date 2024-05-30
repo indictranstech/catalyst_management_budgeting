dcd27ee246ca40f9a1f41b7472f3a8cc7bbab4a3
kiranchoudhary123 <kiranchoudhary0808@gmail.com>
2024-05-30 15:58:13 +0530

    working on time out error

--- catalyst_management/custom_script/project_budgeting/project_budgeting.py
+++ catalyst_management/custom_script/project_budgeting/project_budgeting.py
@@ -23,34 +23,58 @@ def calculating_total_actual_amount_on_update(doc, method):
             start_date = frappe.get_doc("Accounting Period", bam.period).start_date
             end_date = frappe.get_doc("Accounting Period", bam.period).end_date
 
-            pi_amount = frappe.db.get_all("Purchase Invoice Item", {"project_for_budget": doc.name, "budget_account_head": bam.budget_account_head, "docstatus": 1}, ["amount","parent"])
-            si_amount = frappe.db.get_all("Sales Invoice Item", {"project_for_budget": doc.name, "budget_account_head": bam.budget_account_head, "docstatus": 1}, ["amount","parent"])
-            je_amount = frappe.db.get_all("Journal Entry Account", {"project_for_budget": doc.name, "budget_account_head": bam.budget_account_head, "docstatus": 1}, ["debit_in_account_currency","parent"])
-            ec_amount = frappe.db.get_all("Expense Claim Detail", {"project_for_budget": doc.name, "budget_account_head": bam.budget_account_head, "docstatus": 1}, ["amount","parent"])
+            sql_query = """
+                SELECT p.posting_date, i.amount
+                FROM `tabPurchase Invoice Item` i, `tabPurchase Invoice` p 
+                WHERE i.parent = p.name AND i.project_for_budget = %s 
+                AND i.budget_account_head = %s 
+                AND p.docstatus = 1 
+                AND p.posting_date BETWEEN %s AND %s
+            """
+            pi_amount = frappe.db.sql(sql_query, (doc.name, bam.budget_account_head, start_date, end_date), as_dict=True)
 
+            sql_query1 = """
+                SELECT s.posting_date, i.amount 
+                FROM `tabSales Invoice Item` i, `tabSales Invoice` s 
+                WHERE i.parent = s.name AND i.project_for_budget = %s 
+                AND i.budget_account_head = %s 
+                AND s.docstatus = 1 
+                AND s.posting_date BETWEEN %s AND %s
+            """
+            si_amount = frappe.db.sql(sql_query1, (doc.name, bam.budget_account_head, start_date, end_date), as_dict=True)
+
+            sql_query2 = """
+                SELECT j.posting_date, i.debit_in_account_currency 
+                FROM `tabJournal Entry Account` i, `tabJournal Entry` j 
+                WHERE i.parent = j.name AND i.project_for_budget = %s 
+                AND i.budget_account_head = %s 
+                AND j.docstatus = 1 
+                AND j.posting_date BETWEEN %s AND %s
+            """
+            je_amount = frappe.db.sql(sql_query2, (doc.name, bam.budget_account_head, start_date, end_date), as_dict=True)
+
+            sql_query3 = """
+                SELECT e.posting_date, i.amount
+                FROM `tabExpense Claim Detail` i, `tabExpense Claim` e 
+                WHERE i.parent = e.name AND i.project_for_budget = %s 
+                AND i.budget_account_head = %s 
+                AND e.docstatus = 1 
+                AND e.posting_date BETWEEN %s AND %s
+            """
+            ec_amount = frappe.db.sql(sql_query3, (doc.name, bam.budget_account_head, start_date, end_date), as_dict=True)
+
+            
             for pi in pi_amount:
-                pi_doc = frappe.get_doc('Purchase Invoice', pi['parent'])
-                posting_date = pi_doc.posting_date
-                if start_date <= posting_date <= end_date:
-                    pi_amount_total += float(pi.get('amount', 0))
+                pi_amount_total += float(pi.get('amount', 0))
 
             for si in si_amount:
-                si_doc = frappe.get_doc('Sales Invoice', si['parent'])
-                posting_date = si_doc.posting_date
-                if start_date <= posting_date <= end_date:
-                    si_amount_total += float(si.get('amount', 0))
+                si_amount_total += float(si.get('amount', 0))
 
             for je in je_amount:
-                je_doc = frappe.get_doc('Journal Entry', je['parent'])
-                posting_date = je_doc.posting_date
-                if start_date <= posting_date <= end_date:
-                    je_amount_total += float(je.get('debit_in_account_currency', 0))
+                je_amount_total += float(je.get('debit_in_account_currency', 0))
 
             for ec in ec_amount:
-                ec_doc = frappe.get_doc('Expense Claim', ec['parent'])
-                posting_date = ec_doc.posting_date
-                if start_date <= posting_date <= end_date:
-                    ec_amount_total += float(ec.get('amount', 0))
+                ec_amount_total += float(ec.get('amount', 0))
 
             # Add each amount to the total
             bam.custom_total_amount += pi_amount_total + si_amount_total + je_amount_total + ec_amount_total
